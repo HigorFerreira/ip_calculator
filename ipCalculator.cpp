62edb92caeb7cb50da3c768e684f143214b36b5f
Higor Ferreira <hfashigor@hotmail.com>
2020-02-20 00:19:38 -0300

    Testing first ip validator  -- not working

--- ipCalculator.cpp
+++ ipCalculator.cpp
@@ -4,31 +4,55 @@
 
 using namespace std;
 
-// class IP {
-// 	private:
-// 		vector<int> octets;
-// 		vector<string> errors;
-// 		char ipClass;
+class IP {
+	private:
+		vector<int> octets;
+		vector<string> errors;
+		char ipClass;
 		
-// 		string completeIpAddress;
-// 		string classlessIpMask;
-// 		string classlessIpCdir;
-// 	public:
-// };
+		string completeIpAddress;
+		string classlessIpMask;
+		string classlessIpCdir;
+
+		/**
+		 * Função de validação do IP pelo formato
+		 * Retorna verdadeiro para um ip no formato XXX.XXX.XXX.XXX,
+		 * X um dígito decimal, podendo o número de digitos variar
+		 * de 1 à 3.
+		 * Retorna falso caso contrário  * */
+		bool ipFormatVerification(string &ip){
+			smatch ipFormatMatch;
+			regex ipFormatRegex("^[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}$");
+			regex_match(ip, ipFormatMatch, ipFormatRegex);
+			return ipFormatMatch.empty();
+		}
+
+	public:
+
+		/**
+		 * Construtor da classe, no qual todo novo ip é inserido **/
+		IP(string ip){
+			if(ipFormatVerification(ip)){
+				octets.push_back(stoi(ip));
+			}
+			else{
+				errors.push_back("Ip no formato incorreto");
+			}
+		}
+
+		void test(){
+			if(errors.size() == 0){
+				cout<<octets.at(0);
+			}
+			else{
+				cout<<errors.at(0);
+			}
+		}
+};
 
 int main(){
-	string ip = "192.168.0.1";
+	
+	IP ip("192.168.0.1");
 
-	// regex ip_basic_format_regex ("(^(\d{1,3}\.){3}\d{1,3}$)");
-	// regex ip_zero_validation_regex ("((^0\d+)|(\.0\d+))");
-
-	smatch matches;
-	regex reg("^[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}$");
-
-	regex_match(ip, matches, reg);
-
-	cout<<matches.size()<<endl;
-	cout<<matches.str()<<endl<<endl;
-	cout<<matches[0]<<endl;
-	cout<<matches[1]<<endl;
+	ip.test();
 }
